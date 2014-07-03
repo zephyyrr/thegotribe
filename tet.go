37553943f4aeb006c81da43618398f4ec7563658
Zephyyrr <zapmew@gmail.com>
2014-07-03 13:45:15 +0200

    Appears to work now. Frame stream is examplified and tested in examples/tet-tracegaze.

--- tet.go
+++ tet.go
@@ -4,23 +4,24 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
-	"os"
 	"reflect"
-	"strings"
 	"time"
 )
 
-var logger = log.New(os.Stdout, "[thegotribe] ", log.LstdFlags)
+var logger = log.New(ioutil.Discard, "[thegotribe] ", log.LstdFlags)
 
 type EyeTracker struct {
-	conn       io.ReadWriteCloser
-	enc        *json.Encoder
-	dec        *json.Decoder
-	incoming   chan Response
+	conn io.ReadWriteCloser
+	enc  *json.Encoder
+	dec  *json.Decoder
+
+	Frames     chan Frame
 	heartbeats chan Response
-	OnGaze     GazeFunc
+	sets       chan Response
+	interests  map[string][]chan<- interface{}
 }
 
 func Create() (tracker *EyeTracker, err error) {
@@ -33,12 +34,14 @@ func Create() (tracker *EyeTracker, err error) {
 
 func New(conn io.ReadWriteCloser) (tracker *EyeTracker) {
 	tracker = &EyeTracker{
-		conn:       conn,
-		enc:        json.NewEncoder(conn),
-		dec:        json.NewDecoder(conn),
-		incoming:   make(chan Response, 10),
+		conn: conn,
+		enc:  json.NewEncoder(conn),
+		dec:  json.NewDecoder(conn),
+
+		Frames:     make(chan Frame, 1),
 		heartbeats: make(chan Response),
-		OnGaze:     NullFunc,
+		sets:       make(chan Response),
+		interests:  make(map[string][]chan<- interface{}),
 	}
 	go tracker.readPackets()
 
@@ -71,36 +74,61 @@ func (et *EyeTracker) readPackets() {
 		logger.Println("Raw:", res)
 		if err != nil {
 			logger.Println(err)
-			close(et.incoming)
 			close(et.heartbeats)
+			close(et.Frames)
 			return
+		}
+
+		if res.StatusCode != OK { // Deals with bad status codes
+			continue
 		}
 
 		if res.Category == CategoryHeartbeat {
 			et.heartbeats <- res
+			continue
 		}
 
-		if et.OnGaze == nil {
-			logger.Println("OnGaze == nil")
+		if res.Category == CategoryCalibration {
+			// Not yet supported.
+			continue
 		}
 
-		if frame := res.Values.Frame; frame != nil && et.OnGaze != nil {
-			logger.Println(frame)
-			et.OnGaze(*frame)
+		// Only res.Category == CategoryTracker possible at this point
+		if res.Category != CategoryTracker {
+			panic("Unknown category " + res.Category)
+		}
+
+		if res.Request == RequestSet {
+			et.sets <- res
+			continue
+		}
+
+		// Only res.Request == RequestGet possible at this point
+		if res.Request != RequestGet {
+			panic("Unknown request method " + res.Request)
+		}
+
+		if frame := res.Values.Frame; frame != nil {
+			select {
+			case et.Frames <- *frame:
+			default: //Throw it away. Client is busy.
+			}
 			res.Values.Frame = nil // Handeled
 		}
-		if res.Values.StatusMessage != nil ||
-			res.Values.Push != nil ||
-			res.Values.HeartbeatInterval != nil ||
-			res.Values.Version != nil ||
-			res.Values.TrackerState != nil ||
-			res.Values.Framerate != nil ||
-			res.Values.ScreenIndex != nil ||
-			res.Values.ScreenPsyW != nil ||
-			res.Values.ScreenResW != nil ||
-			res.Values.ScreenResH != nil ||
-			res.Values.ScreenPsyH != nil {
-			et.incoming <- res
+
+		// Deal with every other value of tracker
+		val, typ := reflect.ValueOf(res.Values), reflect.TypeOf(res.Values)
+		for i := 0; i < val.NumField(); i++ {
+			if field := val.Field(i); !field.IsNil() {
+				name := typ.Field(i).Tag.Get("json") // Only if value is present
+				if list, ok := et.interests[name]; ok && len(list) > 0 {
+					for _, ch := range list { // Only if value is present and there are interested parties
+						ch <- field.Elem().Interface() // Notify
+						close(ch)                      // Close
+					}
+					et.interests[name] = list[0:0] //Clear interest list
+				}
+			}
 		}
 	}
 }
@@ -119,19 +147,19 @@ func (et *EyeTracker) heartbeat(interval time.Duration) {
 }
 
 func (et *EyeTracker) Set(attribute string, value interface{}) error {
-	et.SetAll(map[string]interface{}{
+	return et.SetAll(map[string]interface{}{
 		attribute: value,
 	})
-	return nil
+
 }
 
 func (et *EyeTracker) SetAll(attributes map[string]interface{}) error {
 	et.enc.Encode(Request{
 		Category: CategoryTracker,
-		Request:  "set",
+		Request:  RequestSet,
 		Values:   attributes,
 	})
-	res := <-et.incoming
+	res := <-et.sets
 	if res.StatusCode != OK {
 		return res.StatusCode
 	}
@@ -139,40 +167,23 @@ func (et *EyeTracker) SetAll(attributes map[string]interface{}) error {
 }
 
 func (et *EyeTracker) Get(attribute string) (interface{}, error) {
-	res, err := et.GetAll(attribute)
+	resch := make(chan interface{}, 1)
+	et.interests[attribute] = append(et.interests[attribute], resch)
+
+	err := et.enc.Encode(Request{
+		Category: CategoryTracker,
+		Request:  RequestGet,
+		Values:   []string{attribute},
+	})
 
 	if err != nil {
 		return nil, err
-	} else {
-		if val := reflect.ValueOf(res).Elem().FieldByNameFunc(func(name string) bool {
-			return strings.ToLower(name) == attribute
-		}); !val.IsNil() {
-			return val.Elem().Interface(), nil
-		}
-		return nil, errors.New(*res.StatusMessage)
-	}
-}
-
-func (et *EyeTracker) GetAll(attributes ...string) (*Values, error) {
-	et.enc.Encode(Request{
-		Category: CategoryTracker,
-		Request:  "get",
-		Values:   attributes,
-	})
-
-	result, ok := <-et.incoming
-	if !ok {
-		return nil, errors.New("End of Connection")
-	}
-	if result.StatusCode != OK {
-		return &result.Values, result.StatusCode
 	}
 
-	return &result.Values, nil
-}
-
-type GazeFunc func(Frame)
-
-func NullFunc(f Frame) {
-
+	select {
+	case res := <-resch:
+		return res, nil
+	case <-time.After(3 * time.Second):
+		return nil, errors.New("Request timeout")
+	}
 }
